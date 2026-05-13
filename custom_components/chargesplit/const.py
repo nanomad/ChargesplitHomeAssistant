5b994de39171842d2d106052a656451f8ee46876
Giovanni Condello <giovanni.condello@coderit.it>
2026-05-13 17:42:52 +0200

    Remove dead NAME and CONF_ENABLED constants from const.py

--- custom_components/chargesplit/const.py
+++ custom_components/chargesplit/const.py
@@ -1,8 +1,6 @@
 """Constants for Chargesplit."""
-NAME = "Chargesplit Domus"
 DOMAIN = "chargesplit"
 
-CONF_ENABLED = "enabled"
 CONF_CODE = "code"
 CONF_SYNC_INTERVAL = "sync_interval"
 CHARGEPOINT_SERIAL = "serial"
